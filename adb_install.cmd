ed3b0b9323f12db7c72f8b6dcd544b3e9c395452
gfm688 <gfm688@qq.com>
2021-01-23 11:47:04 +0800

    add adb tools for windows

---
+++ adb_install.cmd
@@ -0,0 +1,33 @@
+@echo off
+set PACKAGE_FILE=%1
+if "%PACKAGE_FILE%"=="" set /P PACKAGE_FILE=Input package file: 
+
+pushd %~dp0
+where adb.exe >nul 2>nul
+if %ERRORLEVEL% NEQ 0 (
+setlocal enabledelayedexpansion
+path !CD!\platform-tools;!PATH!
+)
+
+echo Uploading...
+adb push "%PACKAGE_FILE%" /update.zip
+if %ERRORLEVEL% NEQ 0 (
+    goto END
+)
+
+echo Unpacking...
+adb shell unzip -oq update.zip -x README.md
+if %ERRORLEVEL% NEQ 0 (
+    goto END
+)
+adb shell rm /update.zip
+
+echo Installing...
+adb shell chmod +x /install.sh
+adb shell /install.sh
+if %ERRORLEVEL% NEQ 0 (
+    goto END
+)
+adb reboot
+:END
+pause
